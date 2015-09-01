56e0a99280c9833cb58e877e22b532d8ab02c00c
mruibalm <mruibalm@gmail.com>
2015-09-01 23:12:33 +0200

    app nombre

--- app/src/main/java/com/example/usuario/kiman/MainActivity.java
+++ app/src/main/java/com/example/usuario/kiman/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.usuario.kiman;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private Button btnAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Obtenemos una referencia a los controles de la interfaz
+        txtNombre = (EditText)findViewById(R.id.TxtNombre);
+        btnAceptar = (Button)findViewById(R.id.BtnAceptar);
+        //Implementamos el evento click del botón
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creamos el Intent
+                Intent intent =
+                        new Intent(MainActivity.this, SaludoActivity.class);
+
+                //Creamos la información a pasar entre actividades
+                Bundle b = new Bundle();
+                b.putString("NOMBRE", txtNombre.getText().toString());
+
+                //Añadimos la información al intent
+                intent.putExtras(b);
+
+                //Iniciamos la nueva actividad
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
